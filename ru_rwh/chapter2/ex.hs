fb879a49ddcbbe6b58d3b58f62e76ba16b2f7d79
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-13 21:07:45 +0000

    Different approach for lastButOne

--- ru_rwh/chapter2/ex.hs
+++ ru_rwh/chapter2/ex.hs
@@ -2,3 +2,9 @@ lastButOne :: [a] -> a
 lastButOne [] = error "List is empty"
 lastButOne [x] = error "List does not have enough elements"
 lastButOne xs = reverse xs !! 1
+
+lastButOne' :: [a] -> a
+lastButOne' [] = error "List is empty"
+lastButOne' [x] = error "List does not have enough elements"
+lastButOne' (x:_:[]) = x
+lastButOne' (_:xs) = lastButOne' xs
