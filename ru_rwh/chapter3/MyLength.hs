47470056dda1cb1be8a361f69b60472e1fae0ffc
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-15 21:30:36 +0000

    Optimized length functions

--- ru_rwh/chapter3/MyLength.hs
+++ ru_rwh/chapter3/MyLength.hs
@@ -4,6 +4,12 @@ myLength = foldr (\_ acc -> acc + 1) 0
 myLengthl :: [a] -> Int
 myLengthl = foldl (\acc _ -> acc + 1) 0
 
+myLengthlOpt :: [a] -> Int
+myLengthlOpt = foldl' (\acc _ -> acc + 1) 0
+
+myLengthOpt :: [a] -> Int
+myLengthOpt = foldr' (\_ acc -> acc + 1) 0
+
 myLength' :: [a] -> Int
 myLength' [] = 0
 myLength' [x] = 1
