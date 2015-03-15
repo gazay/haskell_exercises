0444dd408c363165dd20af0d5b55d5efac2c38bf
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-15 18:44:36 +0000

    Length through foldl

--- ru_rwh/chapter3/MyLength.hs
+++ ru_rwh/chapter3/MyLength.hs
@@ -1,6 +1,9 @@
 myLength :: [a] -> Int
 myLength = foldr (\_ acc -> acc + 1) 0
 
+myLengthl :: [a] -> Int
+myLengthl = foldl (\acc _ -> acc + 1) 0
+
 myLength' :: [a] -> Int
 myLength' [] = 0
 myLength' [x] = 1
