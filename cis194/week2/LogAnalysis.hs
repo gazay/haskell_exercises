5697781cf562dd5412e75b7f3d473b9dbe0fbe6f
Alexey Gaziev <alex.gaziev@gmail.com>
2015-06-27 06:18:02 +0000

    Add parseLog and parseType functions

--- cis194/week2/LogAnalysis.hs
+++ cis194/week2/LogAnalysis.hs
@@ -1,4 +1,21 @@
-{-# OPTIONS_GHC -WALL #-}
+{-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
 import Log
+
+parseLog :: String -> LogMessage
+parseLog line = let (t,rest) = parseType line
+                    (stamp,msg) = break (== ' ') . tail $ rest
+                    numStamp = read stamp :: TimeStamp
+                in
+                  LogMessage t numStamp (tail msg)
+
+parseType :: String -> (MessageType, String)
+parseType str = let (t,rest) = break (== ' ') str
+                    (errLevel,errRest) = break (== ' ') . tail $ rest
+                in
+                  case t of
+                    "I" -> (Info,rest)
+                    "W" -> (Warning,rest)
+                    "E" -> (Error errLevel,rest)
+                    _   -> error "Wrong input"
